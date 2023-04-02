7c58fcae2705dfee25b8a8b5a8597040a4d76fbf
Erdenechuluun Khuderchuluun <tesoro.ec@gmail.com>
2023-04-03 01:44:39 +0800

    done

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,11 @@ import { Cube } from './shapes/Cube';
 import { Sphere } from './shapes/Sphere';
 import { Cylinder } from './shapes/Cylinder';
 import DrawingCanvas from './pages/DrawingCanvas';
-
-import img from './textures/cubetexture.png'
-import texture from './textures/textures.jpg'
-import texture2 from './textures/texture2.jpg'
+import Texture from './textures/textures.jpg'
+import Texture1 from './textures/texture1.jpg'
+import Texture2 from './textures/texture2.jpg'
 import Property from './pages/Property';
-
+//npm install & npm start
 interface randomProperty{
   cube:number,
   sphere:number,
@@ -18,21 +17,25 @@ interface randomProperty{
 
 
 function App() {
-
-  
   const [socket, setSocket] = useState<WebSocket|null>(null);
   const [cube, setCube] = useState<number[]>(Cube.vertices);
   const [sphere, setSphere] = useState<number[]>(Sphere.vertices)
   const [cylinder, setCylinder] = useState<number[]>(Cylinder.vertices)
 
+  const [cubeProp, setCubeProp] = useState<number>();
+  const [sphereProp, setSphereProp] = useState<number>()
+  const [cylinderProp, setCylinderProp] = useState<number>()
+
   const handleMessage = (event:MessageEvent) => {
     const randomProp: randomProperty = JSON.parse(JSON.parse(event.data));
-
+    setCubeProp(parseFloat(randomProp.cube.toFixed(3))) 
+    setSphereProp(parseFloat(randomProp.sphere.toFixed(3))) 
+    setCylinderProp(parseFloat(randomProp.cylinder.toFixed(3))) 
     setCube(
       Cube.vertices.map(c=>{
         return c * randomProp.cube;
       })
-    ) 
+    )
     setSphere(
       Sphere.vertices.map(c=>{
         return c * randomProp.sphere;
@@ -43,9 +46,6 @@ function App() {
         return c * randomProp.cylinder;
       })
     ) 
-    
-    
-    
   };
   useEffect(() => {
     const newSocket = new WebSocket("ws://localhost:8000/ws");
@@ -65,16 +65,15 @@ function App() {
     };
 
   }, [socket]);
-
  
   return (
-    <div className='Container'>
-      <Property socket={socket}/>
-      <div className='Shapes'>
-        <h2>Shapes</h2>
-        <DrawingCanvas vertices={cube} indices={Cube.indices} vertexNormals={Cube.vertexNormals} textureCoordinates={Cube.textureCoordinates} width={500} height={500} textImg={texture} />
-        <DrawingCanvas vertices={sphere} indices={Sphere.indices} vertexNormals={Sphere.vertexNormals} textureCoordinates={Sphere.textureCoordinates} width={500} height={500} textImg={texture2}  />
-        <DrawingCanvas vertices={cylinder} indices={Cylinder.indices} vertexNormals={Cylinder.vertexNormals} textureCoordinates={Cylinder.textureCoordinates} width={500} height={500} textImg={img}/>
+    <div className='container'>
+      <Property socket={socket} cube={cubeProp} sphere={sphereProp} cylinder={cylinderProp}/>
+      <div>
+        <div className='shapes'><h2>Shapes</h2></div>
+        <DrawingCanvas vertices={cube} indices={Cube.indices} vertexNormals={Cube.vertexNormals} textureCoordinates={Cube.textureCoordinates} width={500} height={500} textImg={Texture} />
+        <DrawingCanvas vertices={sphere} indices={Sphere.indices} vertexNormals={Sphere.vertexNormals} textureCoordinates={Sphere.textureCoordinates} width={500} height={500} textImg={Texture2}  />
+        <DrawingCanvas vertices={cylinder} indices={Cylinder.indices} vertexNormals={Cylinder.vertexNormals} textureCoordinates={Cylinder.textureCoordinates} width={500} height={500} textImg={Texture1}/>
       </div>
     </div>
   );
