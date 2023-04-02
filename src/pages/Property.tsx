6e2cb1be25adbaa32a07e2dd864501a71e562476
Erdenechuluun Khuderchuluun <tesoro.ec@gmail.com>
2023-04-03 01:55:28 +0800

    done

--- src/pages/Property.tsx
+++ src/pages/Property.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 import '../App.css';
 interface propertyProps{
-    socket: WebSocket|null
+    socket: WebSocket|null,
+    cube?:number,
+    sphere?:number,
+    cylinder?:number,
 }
-const Property = ({socket}:propertyProps) => {
-
+const Property = ({socket, cube, sphere, cylinder}:propertyProps) => {
     const handleSubmit = (event:React.FormEvent) => {
         event.preventDefault();
         if (!socket) {
@@ -14,32 +16,24 @@ const Property = ({socket}:propertyProps) => {
         socket.send(JSON.stringify({value:true }));
       };
    
-
-
-
   return (
     <div>
         <h2>Properties</h2>
             <form  onSubmit={handleSubmit}>
-    
                 <div className='properties'>  
                     <label htmlFor="side">Cube</label>
-                    <input disabled type="number" name="side" id="side"  placeholder='side'/>
+                    <input disabled type="number" name="side" id="side" value={cube}  placeholder='side'/>
                 </div>
-        
                 <div className='properties'>  
                     <label htmlFor="radius">Sphere</label>
-                    <input disabled type="number" name="radius" id="radius"  placeholder='radius' />
+                    <input disabled type="number" name="radius" id="radius" value={sphere} placeholder='radius' />
                 </div>
-    
-  
-          
                 <div className='cylinder'>  
                     <label htmlFor="cylinder">Cylinder</label>
-                    <input disabled type="number" name="radius" id="cylinder" placeholder='radius'/>
-                    <input disabled type="number" name="height" id="cylinder-height" placeholder='height' />
+                    <input disabled type="number" name="radius" id="cylinder" placeholder='radius' value={cylinder}/>
+                    <input disabled type="number" name="height" id="cylinder-height" placeholder='height' value={cylinder&&cylinder*2}/>
                 </div>
-             
+
                 <button type="submit">Generate random</button>
             </form>
         </div>
